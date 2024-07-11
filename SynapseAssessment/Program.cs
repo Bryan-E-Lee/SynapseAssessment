ba947e31429c85550e672dca0b73e013b197f851
Bryan Lee <bryanlee1990@gmail.com>
2024-07-10 19:13:00 -0500

    Removed unused using statements.

--- SynapseAssessment/Program.cs
+++ SynapseAssessment/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
+using System.Text;
 
 namespace Synapse.OrdersExample
 {
